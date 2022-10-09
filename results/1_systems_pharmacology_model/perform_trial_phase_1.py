c459208b15ebdba893ae2ca5b6caeae0ecd83059
DavAug <david.augustin@gmx.net>
2022-10-09 09:47:04 +0100

    fix covariate distribution

--- results/1_systems_pharmacology_model/perform_trial_phase_1.py
+++ results/1_systems_pharmacology_model/perform_trial_phase_1.py
@@ -37,35 +37,35 @@ def define_demographics(n):
     after Hamberg et al (2011).
     """
     seed = 1234
-    n_cov = 5  # (1, 4, 5) all refer to the VKORC1 genotype
+    n_cov = 5  # (1, 2, 5) all refer to the VKORC1 genotype
     covariates = np.zeros(shape=(n, n_cov))
 
     n_cyp2p9_33 = int(np.ceil(0.006 * n))
-    covariates[:n_cyp2p9_33, 1] += 1
+    covariates[:n_cyp2p9_33, 2] += 1
     n_cyp2p9_23 = int(np.ceil(0.012 * n))
-    covariates[:n_cyp2p9_33+n_cyp2p9_23, 1] += 1
+    covariates[:n_cyp2p9_33+n_cyp2p9_23, 2] += 1
     n_cyp2p9_22 = int(np.ceil(0.014 * n))
-    covariates[:n_cyp2p9_33+n_cyp2p9_23+n_cyp2p9_22, 1] += 1
+    covariates[:n_cyp2p9_33+n_cyp2p9_23+n_cyp2p9_22, 2] += 1
     n_cyp2p9_13 = int(np.ceil(0.123 * n))
-    covariates[:n_cyp2p9_33+n_cyp2p9_23+n_cyp2p9_22+n_cyp2p9_13, 1] += 1
+    covariates[:n_cyp2p9_33+n_cyp2p9_23+n_cyp2p9_22+n_cyp2p9_13, 2] += 1
     n_cyp2p9_12 = int(np.ceil(0.184 * n))
     covariates[
-        :n_cyp2p9_33+n_cyp2p9_23+n_cyp2p9_22+n_cyp2p9_13+n_cyp2p9_12, 1] += 1
+        :n_cyp2p9_33+n_cyp2p9_23+n_cyp2p9_22+n_cyp2p9_13+n_cyp2p9_12, 2] += 1
 
     typical_age = 68
     np.random.seed(seed)
-    covariates[:, 2] = np.random.lognormal(
+    covariates[:, 3] = np.random.lognormal(
         mean=np.log(typical_age), sigma=0.1, size=n)
 
     # (VKORC does not affect PK, but we just synthethise them for completeness)
     n_vkorc1_AA = int(np.ceil(0.15 * n))
-    covariates[:n_vkorc1_AA, [0, 3, 4]] += 1
+    covariates[:n_vkorc1_AA, [0, 1, 4]] += 1
     n_vkorc1_GA = int(np.ceil(0.485 * n))
     covariates[:n_vkorc1_AA+n_vkorc1_GA, [0, 3, 4]] += 1
 
     # Shuffle CYP-VKORC pairs
     indices = np.random.choice(np.arange(n), replace=False, size=n)
-    covariates[:, 1] = covariates[indices, 1]
+    covariates[:, 2] = covariates[indices, 2]
 
     return covariates
 
