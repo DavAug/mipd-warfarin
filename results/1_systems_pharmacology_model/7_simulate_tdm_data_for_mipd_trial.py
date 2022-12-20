7ad979e9a8e1f45c9dda8f36fea5b28dd11e58bc
DavAug <david.augustin@gmx.net>
2022-12-20 17:08:41 +0100

    implement IOV and EV in trial simulation

--- results/1_systems_pharmacology_model/7_simulate_tdm_data_for_mipd_trial.py
+++ results/1_systems_pharmacology_model/7_simulate_tdm_data_for_mipd_trial.py
@@ -9,18 +9,7 @@ import pandas as pd
 from model import define_wajima_model
 
 
-def define_model():
-    """
-    Defines Wajima's model of the coagulation network.
-    """
-    model, _ = define_wajima_model(patient=True, inr_test=True)
-    error_model = chi.LogNormalErrorModel()
-    model = chi.PredictiveModel(model, error_model)
-
-    return model
-
-
-def get_regimen(patient, n):
+def get_regimen(patient, n, delays):
     """
     Imports the patient's dosing regimen.
     """
@@ -47,41 +36,61 @@ def get_regimen(patient, n):
     doses = df[mask][['Dose %d in mg' % (d+1) for d in range(n-1)]].values[0]
 
     # Define dosing regimen
-    cal_time = 100
+    regimen = define_dosing_regimen(doses, delays[:n])
+
+    return regimen
+
+
+def define_dosing_regimen(doses, delays, cal_time=100*24):
+    """
+    Returns a dosing regimen with delayed administration times.
+    """
     duration = 0.01
-    dose_rates = doses / duration
     regimen = myokit.Protocol()
-    for idx, dr in enumerate(dose_rates):
-        if dr == 0:
+    for day, dose in enumerate(doses):
+        if dose == 0:
             continue
         regimen.add(myokit.ProtocolEvent(
-            level=dr,
-            start=(cal_time + idx) * 24,
+            level=dose/duration,
+            start=cal_time+day*24+delays[day],
             duration=duration))
 
     return regimen
 
 
-def generate_measurement(model, patient, n):
+def generate_measurement(model, patient, n, delays, vk_input, rng):
     """
     Generates TDM data for individual.
     """
-    # Get patient ID (use this as seed for RNG)
-    seed = int(patient.ID)
-
     # Get parameters of indvidual
-    parameters = np.array(patient[model.get_parameter_names()].values)
+    parameters = np.array(patient[model.parameters()].values)
+    sigma_log = np.array([patient['Sigma log']])
 
     # Simulate measurements
     # NOTE nth measurement occurs on (n-1)th day. And we let the system
     # equilibrate for 100 days.
     cal_time = 100
-    times = [(cal_time + n - 1) * 24]
-    measurement = model.sample(
-        parameters=parameters, times=times, seed=seed, return_df=False
-    )[0, 0, 0]
+    times = np.array([0 ,(n - 1) * 24 + delays[n]]) + cal_time * 24
+    inr = model.simulate(
+        parameters=parameters, times=times, vk_input=vk_input[:n])[:, -1]
+
+    # Add noise
+    error_model = chi.LogNormalErrorModel()
+    measurement = error_model.sample(
+        parameters=sigma_log, model_output=inr, seed=rng)[0, 0]
 
     return measurement
+
+
+def get_vk_consumption(days, nids, seed):
+    """
+    Returns deviations of the vk consumption from the mean consumption drawn
+    from a normal distribution of shape (days, n_ids).
+    """
+    rng = np.random.default_rng(seed)
+    vk_input = rng.normal(loc=1, scale=0.1, size=(days, nids))
+
+    return vk_input
 
 
 def save_results(ids, measurements, n):
@@ -140,19 +149,34 @@ if __name__ == '__main__':
         raise ValueError('Invalid number.')
     n = args.number
 
+    # Define number of days for simulation
+    days = 19
+    if n > days:
+        raise ValueError(
+            'Invalid number. The script is set to 19 trial days. Therefore '
+            'n cannot exceed 19.')
+
     # Load patient data
     directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     data = pd.read_csv(directory + '/data/mipd_trial_cohort.csv')
 
     # Define model
-    model = define_model()
+    model, _ = define_wajima_model(patient=True, inr_test=True)
+
+    # Define IOV and EV
+    ids = data.ID.unique()
+    rng = np.random.default_rng(seed=4)
+    delays = rng.exponential(scale=0.5, size=(days, len(ids)))
+    vk_input = get_vk_consumption(days, len(ids), seed=14)
 
     # Simulate measurements
-    ids = data.ID.unique()
+    rng = np.random.default_rng(seed=len(ids)*n)
     measurements = np.empty(shape=len(ids))
     for idp, patient in data.iterrows():
-        r = get_regimen(patient, n)
+        d = delays[:, idp]
+        vk = vk_input[:, idp]
+        r = get_regimen(patient, n, d)
         model.set_dosing_regimen(r)
-        measurements[idp] = generate_measurement(model, patient, n)
+        measurements[idp] = generate_measurement(model, patient, n, d, vk, rng)
 
     save_results(ids, measurements, n)
