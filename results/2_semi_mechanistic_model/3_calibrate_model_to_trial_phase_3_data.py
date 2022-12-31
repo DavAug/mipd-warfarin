0675040b0f5c50ddd698368665b2ce8f3bbf7c8d
DavAug <david.augustin@gmx.net>
2022-12-31 15:51:55 +0100

    fix calibration to ph 2

--- results/2_semi_mechanistic_model/3_calibrate_model_to_trial_phase_3_data.py
+++ results/2_semi_mechanistic_model/3_calibrate_model_to_trial_phase_3_data.py
@@ -28,27 +28,31 @@ def define_log_posterior():
     population_model = define_steady_state_hamberg_population_model(
         centered=False)
     log_prior = pints.ComposedLogPrior(
-        pints.GaussianLogPrior(-3.658, 0.029),     # Mean log k_e
-        pints.GaussianLogPrior(0.096, 0.02),       # Sigma log k_e
-        pints.GaussianLogPrior(0.479, 0.05),       # Rel. shift k_e CYP29P *2
-        pints.GaussianLogPrior(0.74, 0.056),       # Rel. shift k_e CYP29P *3
-        pints.GaussianLogPrior(0.0063, 0.0028),    # Rel. shift k_e Age
-        pints.GaussianLogPrior(0.755, 0.14),       # Mean log EC50
-        pints.LogNormalLogPrior(-2.117, 0.31),     # Sigma log EC50
-        pints.GaussianLogPrior(0.563, 0.066),      # Rel. shift EC50 VKORC1 A
-        pints.GaussianLogPrior(2.66, 0.021),       # Mean log volume
-        pints.GaussianLogPrior(0.0949, 0.015),     # Sigma log volume
+        pints.GaussianLogPrior(0.073, 0.028),      # Mean log baseline INR G
+        pints.GaussianLogPrior(0.170, 0.016),      # Std. log baseline INR
+        pints.GaussianLogPrior(1.043, 0.056),      # Mean log baseline INR A
+        pints.GaussianLogPrior(-3.738, 0.027),     # Mean log ke
+        pints.GaussianLogPrior(0.116, 0.02),       # Sigma log ke
+        pints.GaussianLogPrior(0.571, 0.04),       # Rel. shift ke CYP29P *2
+        pints.GaussianLogPrior(0.823, 0.04),       # Rel. shift ke CYP29P *3
+        pints.GaussianLogPrior(0.00157, 0.0027),   # Rel. shift ke Age
+        pints.GaussianLogPrior(1.471, 0.055),      # Mean log EC50
+        pints.GaussianLogPrior(0.208, 0.028),      # Sigma log EC50
+        pints.GaussianLogPrior(0.532, 0.041),      # Rel. shift EC50 VKORC1 A
+        pints.GaussianLogPrior(2.662, 0.020),      # Mean log volume
+        pints.LogNormalLogPrior(-2.31, 0.16),      # Sigma log volume
+        pints.GaussianLogPrior(0.105, 0.003)       # Sigma log INR
     )
     problem = chi.ProblemModellingController(mechanistic_model, error_model)
     problem.set_population_model(population_model)
     problem.set_data(measurements_df, output_observable_dict={
         'myokit.inr': 'INR'})
-    problem.fix_parameters({
-        'Log mean myokit.baseline_inr': 0.265,
-        'Log std. myokit.baseline_inr': 0.172,
-        'Rel. baseline INR A': 1.251,
-        'Pooled Sigma log': 0.185
-    })
+    # problem.fix_parameters({
+    #     'Log mean myokit.baseline_inr': 0.265,
+    #     'Log std. myokit.baseline_inr': 0.172,
+    #     'Rel. baseline INR A': 1.251,
+    #     'Pooled Sigma log': 0.185
+    # })
     problem.set_log_prior(log_prior)
 
     return problem.get_log_posterior()
